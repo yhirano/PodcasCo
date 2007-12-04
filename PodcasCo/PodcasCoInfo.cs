9bdc05764d01f34458dcd1d262689136d0e40165
hirano <devnull@localhost>
2007-12-04 11:27:46 +0000

    アイコンの修正

--- PodcasCo/PodcasCoInfo.cs
+++ PodcasCo/PodcasCoInfo.cs
@@ -29,7 +29,7 @@ namespace PodcasCo
         /// <summary>
         /// アプリケーションのバージョン
         /// </summary>
-        private const string VERSION_NUMBER = "0.8 TestRelease3";
+        private const string VERSION_NUMBER = "0.8 TestRelease4";
 
         /// <summary>
         /// アプリケーションのバージョン
